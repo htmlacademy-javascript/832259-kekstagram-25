522b5f3c780fdfd15b85efd5c582129e794386f7
Anton <goddef@mail.ru>
2022-02-21 00:53:14 +0300

    Сделал задание

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Первую функцию взял из ссылки и доработал :) https://learn.javascript.ru/task/random-int-min-max
+
+const getRandomInt = (min, max) => {
+  const rand = min + Math.random() * (max - min);
+  if(max > min) {
+    return Math.round(rand);
+  } else if (max < min || max === min) {
+    return 'Недопустимое значение';
+  }
+};
+
+const checkStringLength = (str, length) => {
+  const stringLength = str.length <= length ? 'Длина прошла проверку' : 'Длина не прошла проверку';
+  return stringLength;
+};
+
+getRandomInt(20, 20);
+checkStringLength('fffffff', 6);
