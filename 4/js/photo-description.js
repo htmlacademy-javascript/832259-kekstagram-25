cbb52404431aab9a3a65bd420fd73579e723190b
Keks <keks@htmlacademy.ru>
2022-03-05 02:12:50 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/photo-description.js
@@ -0,0 +1,25 @@
+import {createCommentsList} from './comments.js';
+import {getRandomInt} from './util.js';
+
+
+const COUNT_DESCRIPTION = 25;
+const MIN_VALUE_LIKE = 15;
+const MAX_VALUE_LIKE = 200;
+
+const createPhotoDescription = (amount) => {
+  const photoDescription = [];
+
+  for (let i = 1; i <= amount; i++) {
+    photoDescription.push({
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: 'Описание фотографии',
+      likes: getRandomInt(MIN_VALUE_LIKE, MAX_VALUE_LIKE),
+      comments: createCommentsList()
+    });
+  }
+
+  return photoDescription;
+};
+
+createPhotoDescription(COUNT_DESCRIPTION);
