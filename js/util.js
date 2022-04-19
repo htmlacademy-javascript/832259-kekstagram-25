f2f529c5a3ea95e25d6933e634b3817391fe7796
Anton <goddef@mail.ru>
2022-04-20 02:27:01 +0300

    Удалил ненужное

--- js/util.js
+++ js/util.js
@@ -40,19 +40,6 @@ function debounce (callback, timeoutDelay = 500) {
   };
 }
 
-function throttle (callback, delayBetweenFrames) {
-  let lastTime = 0;
-
-  return (...rest) => {
-    const now = new Date();
-
-    if (now - lastTime >= delayBetweenFrames) {
-      callback.apply(this, rest);
-      lastTime = now;
-    }
-  };
-}
-
 export {isEnterKey};
 export {isEscKey};
 export {showAlert};
